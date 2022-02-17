0f280cc2809fb1ff19661935a80041d559a70c7d
jsteemann <jsteemann@users.noreply.github.com>
2022-02-17 13:21:25 +0100

    fix script

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 cd arangodb
 
-changed_filenames_file="$1"
+operation="$1"
+changed_filenames_file="$2"
+
+# temporary file for diffs
+temp_file=".clang-format-$$.reformat.tmp"
+# clean up after ourselves
+trap "rm -f $temp_file" EXIT SIGINT SIGTERM SIGHUP
 
 # output version info
 clang-format --version
@@ -11,33 +17,57 @@ checked=$(wc -l "$changed_filenames_file" | cut -d " " -f 1)
 
 # read $changed_filenames_file line by line and run clang-format on each filename
 echo "checking $checked file(s)..."
-passed=0
 formatted=0
 while read -r file
   do 
     # some .h files are currently misinterpreted as Objective-C files by clang-format,
     # so we pretend that they are .cpp files. this requires piping the input into
     # clang-format as well, unfortunately.
-    git show ":0:$file" | clang-format -Werror -ferror-limit=0 --dry-run --assume-filename=test.cpp -style=file 
-    status=$?
-
-    if [[ $status -eq 0 ]]
+    if [[ "$operation" = "validate" ]]
     then
-      # all good
-      passed="${passed+1}"
-    elif [[ $status -eq 1 ]]
+      # force .cpp ending to work around clang-format language interpretation
+      nicename="$(basename "$file").cpp"
+      git show ":0:$file" | clang-format -Werror -ferror-limit=0 --dry-run --assume-filename="$nicename" -style=file 
+      status=$?
+      if [[ $status -eq 0 ]]
+      then
+        # all good
+      elif [[ $status -eq 1 ]]
+      then
+        echo "file needs reformatting: $file"
+        echo
+        formatted="$((formatted+1))"
+      else
+        echo "unknown error formatting file: $file"
+        exit 2
+      fi
+    elif [[ "$operation" = "format" ]]
     then
-      echo "file needs reformatting: $file"
-      formatted="${formatted+1}"
+      cat "$file" | clang-format --assume-filename=test.cpp -style=file > "$temp_file" 
+      if [[ "$?" != "0" ]]; then
+        echo "error formatting $file"
+        exit 1
+      fi
+      diff -q "$file" "$temp_file" > /dev/null
+      if [[ "$?" != "0" ]]; then
+        echo "reformatting $file"
+        # move is atomic if we are in the same filesystem (hope we are), so we won't
+        # end up with halfway written files on reformat if the script is killed
+        mv "$temp_file" "$file"
+        formatted="$((formatted+1))"
+      fi
     else
-      echo "unknown error formatting file: $file"
+      echo "unknown operation type: $operation"
       exit 2
     fi
+
   done < "$changed_filenames_file" 
 
 echo
 echo "done - checked $checked file(s), reformatted $formatted file(s)"
-if [[ "$formatted" != "0" ]]; then
-  echo "erroring out because $formatted file(s) needs to be reformatted!!!"
+if [[ "$formatted" != "0" ]] && [[ "$operation" = "validate" ]]
+then
+  echo "erroring out because $formatted file(s) need(s) to be reformatted!!!"
+  echo
   exit 1
 fi
